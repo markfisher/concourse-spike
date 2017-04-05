0d9d6f1641f4d3856f831f64c94b350597aa26ea
markfisher <mfisher@pivotal.io>
2017-04-05 10:13:00 -0400

    add generate-maven-repo task

--- build-image/build-image.sh
+++ build-image/build-image.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-java -jar -Dthin.root=. -Dthin.dryrun target/thindemo-1.0.0.BUILD-SNAPSHOT.jar
-
-ls -alR
+ls -alR repository
 
---
+++ generate-maven-repo/generate-maven-repo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar -Dthin.root=. -Dthin.dryrun target/thindemo-1.0.0.BUILD-SNAPSHOT.jar
+
